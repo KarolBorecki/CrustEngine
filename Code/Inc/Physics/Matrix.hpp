dde7d21e219292d06f9c0b46c336b7e8fa6821de
Karol Borecki <karol.borecki@onet.eu>
2023-03-05 18:35:41 +0100

    Created new matrix implementation with operators overloading

--- Code/Inc/Physics/Matrix.hpp
+++ Code/Inc/Physics/Matrix.hpp
@@ -4,296 +4,389 @@
 #include <vector>
 #include <string>
 
-// TODO reasearch for operands overloading
 /**
-* @brief Represents the mathematical matrix and is able to preform matrix calculations.
-*/
-template<class T>
-class Matrix {
+ * @brief Represents the mathematical matrix and is able to preform matrix calculations.
+ */
+template <class T>
+class Matrix
+{
 public:
   /**
-  * @param rows Amount of rows in the matrix.
-  * @param columns Amount of columns in the matrix.
-  */
-  Matrix(uint8_t rows, uint8_t columns);
+   * @brief This constructor does not reserves the memory for the matrix's array. It only creates memory space for matrix pointer
+   */
+  Matrix();
+
   /**
-  * @param rows Amount of rows in the matrix.
-  * @param columns Amount of columns in the matrix.
-  * @param defaultVal The value that will be placed on each position in matrix.
-  */
-  Matrix(uint8_t rows, uint8_t columns, T defaultVal);
+   * @brief If any argument is zero then no space for matrix's array will be allocated.
+   * @param Height Amount of Height in the matrix.
+   * @param Width Amount of Width in the matrix.
+   */
+  Matrix(uint32_t height, uint32_t width); // TODO 8bits for matrix size is too low
   /**
-  * @details If #mat is not null deletes it from the heap.
-  */
+   * @brief If width or height argument is zero then no space for matrix's array will be allocated.
+   * @param Height Amount of Height in the matrix.
+   * @param Width Amount of Width in the matrix.
+   * @param defaultVal The value that will be placed on each position in matrix.
+   */
+  Matrix(uint32_t height, uint32_t width, T defaultVal);
+  /**
+   * @details If #mat is not null deletes it from the heap.
+   */
   ~Matrix();
 
   /**
-  * @brief Getter for #rows field.
-  *
-  * @return Amount of rows in matrix.
-  */
-  uint8_t Rows();
+   * @brief Getter for #Height field.
+   *
+   * @return Amount of Height in matrix.
+   */
+  uint8_t Height() const;
   /**
-  * @brief Getter for #columns field.
-  *
-  * @return Amount of columns in matrix
-  */
-  uint8_t Columns();
+   * @brief Getter for #Width field.
+   *
+   * @return Amount of Width in matrix
+   */
+  uint8_t Width() const;
 
   /**
-  * @brief Getter for #mat field.
-  *
-  * @return Pointer to matrix array.
-  */
-  double* GetMatrixArray();
-
-  /**
-  * @brief Returns value stored on the given position in matrix.
-  *
-  * @param row The y index of the position in matrix.
-  * @param column The x index of the position in matrix.
-  */
-  double GetValue(uint8_t row, uint8_t column);
-
-  /**
-  * @brief Puts the given value on the given position.
-  *
-  * @param row The y index of the position in matrix.
-  * @param column The x index of the position in matrix.
-  */
-  void PutValue(uint8_t row, uint8_t column, T value);
-
-  /**
-  * @brief Function resets the matrix so each value of it is the same.
-  *
-  * @param value The value that will be placed on each position in matrix.
-  */
+   * @brief Function resets the matrix so each value of it is the same.
+   *
+   * @param value The value that will be placed on each position in matrix.
+   */
   void ResetMatrix(T value);
 
   /**
-  * @brief Adds to given matrix #mat1 another matrix #mat2.
-  *
-  * @param mat1 Modified matrix.
-  * @param mat2 Added matrix.
-  */
-  static void Add(Matrix<T>* mat1, Matrix<T>* mat2);
-
-  /**
-  * @brief Adds to given matrix scalar, constant value.
-  *
-  * @param mat Matrix to which values scalar will be added.
-  * @param scalar Value which will be added.
-  */
-  static void Add(Matrix<T> *mat, const T scalar);
-
-  /**
-   * @brief Substracts from given matrix #mat1 another matrix #mat2.
+   * @brief Getter for matrix ID.
    *
-   * @param mat1 Modified matrix.
-   * @param mat2 Added matrix.
+   * @return Matrix's ID.
    */
-  static void Substract(Matrix<T> *mat1, Matrix<T> *mat2);
-
-  /**
-  * @brief Multiplies given matrixes.
-  *
-  * @details Multiplies matrixes as the mat1 * mat2 and returns new object of type matrix being te result of multiplication.
-  * The original mat1 and mat2 objects remains not modified by this method.
-  *
-  * @param mat1 First matrix.
-  * @param mat2 Second matrix, by which first is being multiplied.
-  * @param outMat The output matrix.
-  */
-  static void Multiply(Matrix<T> *mat1, Matrix<T> *mat2, Matrix<T> &outMat);
-  /**
-  * @brief Multiplies given matrix by scalar constant value.
-  *
-  * @param mat Multiplied Matrix.
-  * @param scalar Value by which matrix is being multiplied.
-  */
-  static void Multiply(Matrix<T> *mat, const T scalar);
-
-  /**
-  * @brief Divides given matrix by scalar constant value.
-  *
-  * @param mat Divided Matrix.
-  * @param scalar Value by which matrix is being divided.
-  */
-  static void Divide(Matrix<T> *mat, const T scalar);
-
-  /**
-  * @brief Converts matrix to std::string, so it can be written on any output.
-  *
-  * @details The returned string is in a following format:<br>
-  * First line contains information about the size of the matrix as follows: Matrix of size: <#rows>x<#columns>.<br>
-  * Next lines contains the matrix in the correct order of rows and columns.
-  */
-  std::string ToString();
-
-  /**
-  * @brief Getter for matrix ID.
-  *
-  * @return Matrix's ID.
-  */
   uint32_t GetID();
 
+  /**
+   * @brief Converts matrix to std::string, so it can be written on any output.
+   *
+   * @details The returned string is in a following format:<br>
+   * First line contains information about the size of the matrix as follows: Matrix of size: <#Height>x<#Width>.<br>
+   * Next lines contains the matrix in the correct order of Height and Width.
+   */
+  std::string ToString() const;
+
+  Matrix<T> &operator+(const Matrix<T> &other) noexcept;
+
+  Matrix<T> &operator+(const T value) noexcept;
+
+  Matrix<T> &operator+=(const Matrix<T> &other) noexcept;
+
+  Matrix<T> &operator+=(const T value) noexcept;
+
+  Matrix<T> &operator-(const Matrix<T> &other) noexcept;
+
+  Matrix<T> &operator-(const T value) noexcept;
+
+  Matrix<T> &operator-=(const Matrix<T> &other) noexcept;
+
+  Matrix<T> &operator-=(const T value) noexcept;
+
+  Matrix<T> &operator*(const Matrix<T> &other) noexcept;
+
+  Matrix<T> &operator*(const T value) noexcept;
+
+  Matrix<T> &operator*=(const Matrix<T> &other) noexcept;
+
+  Matrix<T> &operator*=(const T value) noexcept;
+
+  Matrix<T> &operator/(const T value) noexcept;
+
+  Matrix<T> &operator/=(const Matrix<T> &other) noexcept;
+
+  Matrix<T> &operator/=(const T value) noexcept;
+
+  Matrix<T> &operator=(const Matrix<T> &other) noexcept;
+
+  T *operator[](int x) const noexcept;
+
 private:
-  uint8_t rows { 0 }; //!< Amount of rows in the matrix.
-  uint8_t columns { 0 }; //!< Amount of columns in the matrix.
-  T* mat; //!< Array of matrix's values.
+  uint32_t height{0}; //!< Amount of Height in the matrix.
+  uint32_t width{0};  //!< Amount of Width in the matrix.
+  uint32_t totalSize{0};
 
-  uint32_t ID; //!< Matrix's ID.
-  static inline uint32_t nextID { 0 }; //!< Next matrix ID. Also holds amount of matrixes created on engine work. Only for debug purposes.
-
-  /**
-  * @brief Init function for Matrix. It creates all necessarry arrays and fills it with #defaultVal.
-  *
-  * @param newRows Amount of rows in created array.
-  * @param newColumns Amount of columns in created array.
-  * @param defaultVal Value that the matrix will be filled with after creation.
-  */
-  void Init(uint8_t newRows, uint8_t newColumns, const T defaultVal);
-  
-  /**
-   * @brief Returns matrix's index in array of indexes x=row and y=column.
-   * 
-   * @details Matrix holds linear array (In order to save space and for it to be more reliable and less time-consuming). 
-   * But we use 2-dimensional array in the interface so values are accessed by their's xs and ys (As it is in regular matrixes), This method maps x and y to linear space. 
-   * 
-   * @param row - searched value's index's x.
-   * @param column - searched value's index's y.
-  */
-  uint8_t GetMatRealIndex(uint8_t row, uint8_t column);
+  T *mat; //!< Array of matrix's values.
 };
 
-template<class E>
-inline Matrix<E>::Matrix(uint8_t rows, uint8_t columns) : rows(rows), columns(columns) { Init(rows, columns, 0.0); }
+template <class E>
+inline Matrix<E>::Matrix() : height(0), width(0)
+{
+  totalSize = 0;
+}
 
 template <class E>
-inline Matrix<E>::Matrix(uint8_t rows, uint8_t columns, E defaultVal) : rows(rows), columns(columns) { Init(rows, columns, defaultVal); }
+inline Matrix<E>::Matrix(uint32_t _height, uint32_t _width) : height(_height), width(_width)
+{
+  totalSize = _height * _width;
+  if (totalSize == 0)
+    return;
+  mat = new E[totalSize];
+}
+
+template <class E>
+inline Matrix<E>::Matrix(uint32_t _height, uint32_t _width, E _defaultVal) : height(_height), width(_width)
+{
+  totalSize = _height * _width;
+  if (totalSize == 0)
+    return;
+  mat = new E[totalSize];
+
+  ResetMatrix(_defaultVal);
+}
 
 template <class E>
 inline Matrix<E>::~Matrix()
 {
-  delete [] mat;
+  delete[] mat;
 }
 
 template <class E>
-inline uint8_t Matrix<E>::Rows() { return rows; }
-
-template<class E>
-inline uint8_t Matrix<E>::Columns() { return columns; }
-
-template<class E>
-inline double* Matrix<E>::GetMatrixArray() { return mat; }
+inline uint8_t Matrix<E>::Height() const { return height; }
 
 template <class E>
-inline double Matrix<E>::GetValue(uint8_t row, uint8_t column) { return mat[GetMatRealIndex(row, column)]; }
-
-template<class E>
-inline void Matrix<E>::PutValue(uint8_t row, uint8_t column, E value) { mat[GetMatRealIndex(row, column)] = value; }
+inline uint8_t Matrix<E>::Width() const { return width; }
 
 template <class E>
 void Matrix<E>::ResetMatrix(E value)
 {
-  for(uint8_t i=0; i < rows * columns; i++) mat[i] = value;
+  for (uint32_t i = 0; i < height * width; i++)
+    mat[i] = value;
 }
 
 template <class E>
-void Matrix<E>::Add(Matrix<E> *mat1, Matrix<E> *mat2)
+Matrix<E> &Matrix<E>::operator+(const Matrix<E> &other) noexcept
 {
-  if (mat1->Rows() != mat2->Rows() || mat1->Columns() != mat2->Columns())
-    return;
-  for (uint8_t i = 0; i < mat1->Rows(); i++)
-    for (uint8_t j = 0; j < mat1->Columns(); j++)
-      mat1->PutValue(i, j, mat1->GetValue(i, j) + mat2->GetValue(i, j));
-}
+  if (height != other.Height() || width != other.Width())
+    return *this; // TODO add warning
 
-template <class E>
-void Matrix<E>::Substract(Matrix<E> *mat1, Matrix<E> *mat2)
-{
-  if (mat1->Rows() != mat2->Rows() || mat1->Columns() != mat2->Columns())
-    return;
-  for (uint8_t i = 0; i < mat1->Rows(); i++)
-    for (uint8_t j = 0; j < mat1->Columns(); j++)
-      mat1->PutValue(i, j, mat1->GetValue(i, j) - mat2->GetValue(i, j));
-}
-
-template <class E>
-void Matrix<E>::Add(Matrix<E> *mat, const E scalar)
-{
-  for(uint8_t i=0; i < mat->Rows(); i++)
-    for(uint8_t j=0; j < mat->Columns(); j++)
-      mat->PutValue(i, j, mat->GetValue(i, j) + scalar);
-}
-
-template <class E>
-void Matrix<E>::Multiply(Matrix<E> *mat1, Matrix<E> *mat2, Matrix<E> &outMat)
-{
-  uint8_t resultRows = mat1->Rows();
-  uint8_t resultColumns = mat2->Columns();
-
-  if(mat1->Columns() != mat2->Rows()) {
-    Logger::Error("Trying to multiply matrix of size %dx%d by matrix of size %dx%d, which cannot be done!", mat1->Rows(), mat1->Columns(), mat2->Rows(), mat2->Columns());
-    throw std::runtime_error("Matrix multiplication failed!"); //TODO use my own error handler
+  Matrix<E> *result = new Matrix<E>(height, width);
+  for (int i = 0; i < totalSize; i++)
+  {
+    result->mat[i] = mat[i] + other.mat[i];
   }
 
-  for(uint8_t r=0; r<resultRows; r++) {
-    for(uint8_t c=0; c<resultColumns; c++) {
-      double value = 0;
-      for(uint8_t i=0; i<mat2->Rows(); i++) {
-        value += mat1->GetValue(r, i) * mat2->GetValue(i, c);
+  return *result;
+}
+
+template <class E>
+Matrix<E> &Matrix<E>::operator+(const E value) noexcept
+{
+  Matrix<E> *result = new Matrix<E>(height, width);
+  for (int i = 0; i < totalSize; i++)
+  {
+    result->mat[i] = mat[i] + value;
+  }
+  return *result;
+}
+
+template <class E>
+Matrix<E> &Matrix<E>::operator+=(const Matrix<E> &other) noexcept
+{
+  if (height != other.Height() || width != other.Width())
+    return *this; // TODO add warning
+
+  for (int i = 0; i < totalSize; i++)
+  {
+    mat[i] += other.mat[i];
+  }
+
+  return *this;
+}
+
+template <class E>
+Matrix<E> &Matrix<E>::operator+=(const E value) noexcept
+{
+  for (int i = 0; i < totalSize; i++)
+  {
+    mat[i] += value;
+  }
+  return *this;
+}
+
+template <class E>
+Matrix<E> &Matrix<E>::operator-(const Matrix<E> &other) noexcept
+{
+  if (height != other.height || width != other.width)
+    return *this; // TODO add warning
+
+  Matrix<E> *result = new Matrix<E>(height, width);
+  for (int i = 0; i < totalSize; i++)
+  {
+    result->mat[i] = mat[i] - other.mat[i];
+  }
+
+  return *result;
+}
+
+template <class E>
+Matrix<E> &Matrix<E>::operator-(const E value) noexcept
+{
+  Matrix<E> *result = new Matrix<E>(height, width);
+  for (int i = 0; i < totalSize; i++)
+  {
+    result->mat[i] = mat[i] - value;
+  }
+
+  return *result;
+}
+
+template <class E>
+Matrix<E> &Matrix<E>::operator-=(const Matrix<E> &other) noexcept
+{
+  if (height != other.height || width != other.width)
+    return *this; // TODO add warning
+
+  for (int i = 0; i < totalSize; i++)
+  {
+    mat[i] -= other.mat[i];
+  }
+
+  return *this;
+}
+
+template <class E>
+Matrix<E> &Matrix<E>::operator-=(const E value) noexcept
+{
+  for (int i = 0; i < totalSize; i++)
+  {
+    mat[i] -= value;
+  }
+  return *this;
+}
+
+template <class E>
+Matrix<E> &Matrix<E>::operator*(const Matrix<E> &other) noexcept
+{
+  if (width != other.height)
+    return *this; // TODO add warning
+
+  Matrix<E> *result = new Matrix<E>(height, other.width);
+
+  E *matPointer;
+  for (int x = 0; x < other.width; x++)
+  {
+    matPointer = (*result)[x];
+    for (int y = 0; y < height; y++)
+    {
+      E value;
+      value *= 0; // TODO should be zero
+      for (int i = 0; i < other.height; i++)
+      {
+        value += *this[i][y] * other[x][i];
       }
-      outMat.PutValue(r, c, value);
+      matPointer[y] = value;
     }
   }
+
+  return *result;
 }
 
 template <class E>
-void Matrix<E>::Multiply(Matrix<E> *mat, const E scalar)
+Matrix<E> &Matrix<E>::operator*(const E value) noexcept
 {
-  for(uint8_t i=0; i < mat->Rows(); i++)
-    for(uint8_t j=0; j < mat->Columns(); j++)
-      mat->PutValue(i, j, mat->GetValue(i, j) * scalar);
-}
-
-template <class E>
-void Matrix<E>::Divide(Matrix<E> *mat, const E scalar)
-{
-  if(scalar == 0.0) return;
-  for(uint8_t i=0; i < mat->Rows(); i++)
-    for(uint8_t j=0; j < mat->Columns(); j++)
-      mat->PutValue(i, j, mat->GetValue(i, j) / scalar);
-}
-
-template <class E>
-std::string Matrix<E>::ToString()
-{
-  std::string result = "";
-  result += "Matrix<" + std::to_string(GetID()) + "> of size: " + std::to_string(rows) + "x" + std::to_string(columns) + "\n";
-  if(rows*columns > 200) return result + "Too big to display\n"; //TODO move 200 value to variable
-  for(int r=0; r<rows; r++){
-    for(int c=0; c<columns; c++){
-      result += "(" + std::to_string(r) + "," + std::to_string(c) + ")" +std::to_string(GetValue(r, c)) + " ";
-    }
-    result += "\n";
+  Matrix<E> *result = new Matrix<E>(height, width);
+  for (int i = 0; i < totalSize; i++)
+  {
+    result->mat[i] = mat[i] * value;
   }
-  return result;
+  return *result;
 }
 
 template <class E>
-inline uint32_t Matrix<E>::GetID() { return ID; }
-
-template <class E>
-void Matrix<E>::Init(uint8_t newRows, uint8_t newColumns, const E defaultVal)
+Matrix<E> &Matrix<E>::operator*=(const Matrix<E> &other) noexcept
 {
-  ID = nextID++;
-  mat = new E[newRows * newColumns];
+  if (width != other.height)
+    return *this; // TODO add warning
 
-  ResetMatrix(defaultVal);
+  Matrix<E> result(height, other.width);
+  E *matPointer;
+  for (int x = 0; x < other.width; x++)
+  {
+    matPointer = *this[x];
+    for (int y = 0; y < height; y++)
+    {
+      result[x][y] *= 0;
+      for (int i = 0; i < other.height; i++)
+      {
+        result[x][y] += *this[i][y] * other[x][i];
+      }
+    }
+  }
+  *this = result; // TODO it is kind not good
+  return *this;
 }
 
 template <class E>
-inline uint8_t Matrix<E>::GetMatRealIndex(uint8_t row, uint8_t column) { return column + Columns() * row; }
+Matrix<E> &Matrix<E>::operator*=(const E value) noexcept
+{
+  for (int i = 0; i < totalSize; i++)
+  {
+    mat[i] *= value;
+  }
+  return *this;
+}
 
+template <class E>
+Matrix<E> &Matrix<E>::operator/(const E value) noexcept
+{
+  Matrix<E> *result = new Matrix<E>(height, width);
+  for (int i = 0; i < totalSize; i++)
+  {
+    result->mat[i] = mat[i] / value;
+  }
+  return *result;
+}
+
+template <class E>
+Matrix<E> &Matrix<E>::operator/=(const Matrix<E> &other) noexcept
+{
+  if (height != other.height || width != other.width)
+    return *this; // TODO add warning
+
+  for (int i = 0; i < totalSize; i++)
+  {
+    mat[i] /= other.mat[i];
+  }
+
+  return *this;
+}
+
+template <class E>
+Matrix<E> &Matrix<E>::operator/=(const E value) noexcept
+{
+  for (int i = 0; i < totalSize; i++)
+  {
+    mat[i] /= value;
+  }
+  return *this;
+}
+
+template <class E>
+Matrix<E> &Matrix<E>::operator=(const Matrix<E> &other) noexcept
+{
+  if (this == &other)
+    return *this;
+
+  delete[] mat;
+  mat = nullptr;
+
+  height = other.Height();
+  width = other.Width();
+  totalSize = width * height;
+  mat = new E[totalSize];
+
+  memcpy(mat, other.mat, sizeof(E) * totalSize);
+
+  return *this;
+}
+
+template <class E>
+E *Matrix<E>::operator[](int x) const noexcept
+{
+  return mat + ((x * Height()) % totalSize);
+}
 #endif /* _MATRIX_HPP_ */
--- Code/Src/main.cpp
+++ Code/Src/main.cpp
@@ -1,54 +1,99 @@
-#include <cstdlib> /* Plz no - atoi */
+// #include <cstdlib> /* Plz no - atoi */
 
 #include <Logging/Logger.hpp>
+#include <iostream>
 
-#include <Core/Camera.hpp>
-#include <Core/Scene.hpp>
-#include <Core/Core.hpp>
+// #include <Core/Camera.hpp>
+// #include <Core/Scene.hpp>
+// #include <Core/Core.hpp>
 
-#include <Rendering/Mesh.hpp>
-#include <Rendering/Objects/Lightining/DirectionalLight.hpp>
-#include <Rendering/Objects/RenderObject.hpp>
+// #include <Rendering/Mesh.hpp>
+// #include <Rendering/Objects/Lightining/DirectionalLight.hpp>
+// #include <Rendering/Objects/RenderObject.hpp>
 
-#include <Files/MeshLoader.hpp>
+// #include <Files/MeshLoader.hpp>
 
 #include <Physics/Matrix.hpp>
 
-#include <Scripting/SampleScripts/LoopMove.hpp>
-#include <Scripting/SampleScripts/Rotate.hpp>
+// #include <Scripting/SampleScripts/LoopMove.hpp>
+// #include <Scripting/SampleScripts/Rotate.hpp>
 
 // TODO move all implementation to src files
-int main(int argc, char* argv[])
+
+void print_mat(Matrix<double> &mat)
 {
-  Camera cam(90, 0.1, 1000.0);
-  Scene scene("T Scene", &cam);
+  std::string result = "";
+  result += "Matrix<" + std::to_string(mat.GetID()) + "> of size: " + std::to_string(mat.Height()) + "x" + std::to_string(mat.Width()) + "\n";
+  if (mat.Height() * mat.Width() > 320)
+    Logger::Error("Too big matrix");
+  for (int r = 0; r < mat.Height(); r++)
+  {
+    for (int c = 0; c < mat.Width(); c++)
+    {
+      result += "(" + std::to_string(r) + "," + std::to_string(c) + ")" + boost::lexical_cast<std::string>(mat[c][r]) + " ";
+    }
+    result += "\n";
+  }
+  Logger::Info("%s", result.c_str());
+}
 
-  Mesh mesh("Sample mesh");
-  
-  std::string name = argv[1];
-  std::string path = "/Users/karolborecki/Desktop/CrustEngine/meshes/" + name + ".obj";
+int main(int argc, char *argv[])
+{
+  // Camera cam(90, 0.1, 1000.0);
+  // Scene scene("T Scene", &cam);
 
-  MeshLoader::LoadMeshFromFile(path.c_str(), &mesh);
-  RenderObject rObj(&mesh);
+  // Mesh mesh("Sample mesh");
 
-  LoopMove rObjMove(-100, 0, -100, 0, -100, 0, 10, 5, 7);
-  cam.AttachScript(&rObjMove);
-  // Rotate rObjRotation(&rObj);
-  // rObj.AttachScript(&rObjMove);
-  // rObj.AttachScript(&rObjRotation);
+  // std::string name = argv[1];
+  // std::string path = "/Users/karolborecki/Desktop/CrustEngine/meshes/" + name + ".obj";
 
-  Vector3 lightSourceDir(0.6, 0.2, -0.5);
-  DirectionalLight dirLightSource(255, &lightSourceDir);
+  // MeshLoader::LoadMeshFromFile(path.c_str(), &mesh);
+  // RenderObject rObj(&mesh);
 
-  scene.AddObject(&rObj);
-  scene.AddObject(&dirLightSource);
+  // LoopMove rObjMove(-100, 0, -100, 0, -100, 0, 10, 5, 7);
+  // cam.AttachScript(&rObjMove);
+  // // Rotate rObjRotation(&rObj);
+  // // rObj.AttachScript(&rObjMove);
+  // // rObj.AttachScript(&rObjRotation);
 
-  scene.SetLightProjection(true);
+  // Vector3 lightSourceDir(0.6, 0.2, -0.5);
+  // DirectionalLight dirLightSource(255, &lightSourceDir);
 
-  Core core;
-  core.OpenNewWindow(1000, 800, &scene);
+  // scene.AddObject(&rObj);
+  // scene.AddObject(&dirLightSource);
 
-  core.Quit();
+  // scene.SetLightProjection(true);
 
+  // Core core;
+  // core.OpenNewWindow(1000, 800, &scene);
+
+  // core.Quit();
+
+  Matrix<double> t1(3, 3, 10.0);
+  // t1[0][0] = 1;
+  // t1[1][0] = 2;
+
+  // t1[0][1] = 3;
+  // t1[1][1] = 4;
+
+  // t1[0][2] = 5;
+  // t1[1][2] = 6;
+
+  // print_mat(t1);
+
+  Matrix<double> t2(3, 3, 2.0);
+  // t2[0][0] = 6;
+  // t2[1][0] = 5;
+  // t2[2][0] = 4;
+
+  // t2[0][1] = 3;
+  // t2[1][1] = 2;
+  // t2[2][1] = 1;
+
+  // print_mat(t2);
+
+  // print_mat(t1);
+  // print_mat(t2);
+  print_mat(t1 + t2);
   return 0;
 }
