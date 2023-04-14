30576019b357ac5285bbe4b6d4edaf3b5bc81915
Karol Borecki <karol.borecki@onet.eu>
2023-04-14 11:57:29 +0200

    Added Mover script to test InputHandler

--- CrustEngine/Inc/Scripting/SampleScripts/Mover.hpp
+++ CrustEngine/Inc/Scripting/SampleScripts/Mover.hpp
@@ -6,27 +6,44 @@
 #include <Logging/Logger.hpp>
 #include <Input/InputHandler.hpp>
 
-class Mover : public CrustScript {
+#include <Rendering/Objects/Object.hpp>
+
+class Mover : public CrustScript
+{
 public:
   Mover(double _moveSpeedX, double _moveSpeedY, double _moveSpeedZ);
   ~Mover() override {}
 
   void Start() override;
   void Update(double deltaTime) override;
+
 private:
-  double speedX { 0.0 };
-  double speedY { 0.0 };
-  double speedZ { 0.0 };
+  double speedX{0.0};
+  double speedY{0.0};
+  double speedZ{0.0};
 };
 
-inline Mover::LoopMove(double _moveSpeedX, double _moveSpeedY, double _moveSpeedZ) :
-       speedX(_moveSpeedX), speedY(_moveSpeedY), speedZ(_moveSpeedZ) {}
+inline Mover::Mover(double _moveSpeedX, double _moveSpeedY, double _moveSpeedZ) : speedX(_moveSpeedX), speedY(_moveSpeedY), speedZ(_moveSpeedZ) {}
 
 inline void Mover::Start() {}
 
-void Mover::Update(double deltaTime) {
-  if(InputHandler::GetEvent().keyCode == KeyCode.KEY_A) {
-    Logger::Log("DOSTAŁEM AAAAAAAAAAAAAAAAAAAAAAAAA");
+void Mover::Update(double deltaTime)
+{
+  if (InputHandler::GetEvent().type == EVENT_KEY_DOWN)
+  {
+    if (InputHandler::GetEvent().keyCode == KEY_0)
+    {
+      GetObject().GetTransform().Translate(speedX * deltaTime, 0, 0);
+    } else if (InputHandler::GetEvent().keyCode == KEY_1)
+    {
+      GetObject().GetTransform().Translate(-speedX * deltaTime, 0, 0);
+    }else if (InputHandler::GetEvent().keyCode == KEY_3)
+    {
+      GetObject().GetTransform().Translate(0, speedY * deltaTime, 0);
+    }else if (InputHandler::GetEvent().keyCode == KEY_4)
+    {
+      GetObject().GetTransform().Translate(0, -speedY * deltaTime, 0);
+    }
   }
 }
 
